3b15c6ee486048afacef96f11af623d0dd434e65
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-03-23 22:05:50 +0530

    Naam Simran Singh Add: Zero Sum

---
+++ ZeroSum.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class ZeroSum {
+
+    public static void findZeroSumSubarrays(int[] arr) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int sum = 0;
+
+        // To handle the case where subarray starts from index 0
+        map.put(0, new ArrayList<>());
+        map.get(0).add(-1);
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            if (map.containsKey(sum)) {
+                List<Integer> indices = map.get(sum);
+                for (Integer start : indices) {
+                    System.out.println("Subarray found from index " + (start + 1) + " to " + i);
+                }
+            }
+
+            map.putIfAbsent(sum, new ArrayList<>());
+            map.get(sum).add(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {6, 3, -1, -3, 4, -2, 2, 4, 6, -12, -7};
+        findZeroSumSubarrays(arr);
+    }
+}
